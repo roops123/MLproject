5b8d8c3a738e25f267e55555017151230ebf8845
roops123 <rupali123bawne@gmail.com>
2025-05-19 21:59:32 +0530

    Resolving logger.py error.Implementing data ingestion functionality for the project.

--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,13 @@ import logging
 from datetime import datetime
 import os
 
-LOG_FILE=f"{datetime.now().strftime('%d_%m_%y_%H_%M_%S')}.log"
+#LOG_FILE=f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 LOG_PATH=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(LOG_PATH,exist_ok=True)
 
 logging.basicConfig(
     filemode=LOG_FILE,
-    format="[%(asctime)d] -%(lineno)d -%(name)s -%(levelname)s %(message)s",
+    format="[%(asctime)s] -%(lineno)d -%(name)s -%(levelname)s %(message)s",
     level=logging.INFO
 )
